1f3fda390dbaa1ab6cbc049ad5d696e20b80e174
Hubert Osipowicz <hubert.osipowicz@wiosna.org.pl>
2025-04-14 14:43:17 +0200

    Created gp-frontend project

---
+++ src/components/CurrentEvent.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h2>Aktualne wydarzenie</h2>
+    <div v-if="event">
+      <pre>{{ event }}</pre>
+    </div>
+    <div v-else>Ładowanie danych...</div>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+import axios from 'axios'
+
+const event = ref(null)
+
+onMounted(async () => {
+  try {
+    const response = await axios.get('http://localhost:4010/api/v1/events/aut/reward_categories')
+    event.value = response.data
+  } catch (error) {
+    console.error('Błąd przy pobieraniu danych:', error)
+  }
+})
+</script>
